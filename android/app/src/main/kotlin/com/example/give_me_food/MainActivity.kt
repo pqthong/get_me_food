7fa3b17e5dccad3572d3ebf7b81f14da0790a197
thongpq <thongpq@d-soft.com.vn>
2022-12-11 20:23:47 +0700

    initial

---
+++ android/app/src/main/kotlin/com/example/give_me_food/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.give_me_food
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home/main_food_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class MainFoodPage extends StatefulWidget {
+  const MainFoodPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainFoodPage> createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
